6c8054392848b09b4b85fa80a0bb874a61b47120
Hussein Nasser <hus.mhd@gmail.com>
2018-11-23 21:07:34 -0800

    lecture15

---
+++ index.py
@@ -0,0 +1,20 @@
+import tornado.web
+import tornado.ioloop
+import json
+
+class listRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+        fh = open("list.txt", "r")
+        fruits = fh.read().splitlines()
+        fh.close()
+        self.write(json.dumps(fruits))
+        
+if __name__ == "__main__":
+    app = tornado.web.Application([
+        (r"/list", listRequestHandler)
+    ])
+    
+    port = 8882
+    app.listen(port)
+    print(f"Application is ready and listening on port {port}")
+    tornado.ioloop.IOLoop.current().start()
